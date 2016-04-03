457d2c0afe31761923768bc063ae1caca30ccd38
elina1490 <elinanon@gmail.com>
2016-04-03 18:51:49 +0300

    Added files via upload

---
+++ BasicCalculator/askisi3hm9/Askisi3Hm9.java
@@ -0,0 +1,14 @@
+
+package askisi3hm9;
+import javax.swing.*;
+
+
+public class Askisi3Hm9 {
+
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        Calculator cal  = new Calculator ();
+   
+    }
+}
